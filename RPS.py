d7549fc5c2c8e7b903b88c223b63649b0a877ab6
Lipim-Cervi <felipecervi173@gmail.com>
2026-02-19 22:14:11 +0000

    python
    Co-authored-by: Ona <no-reply@ona.com>

--- RPS.py
+++ RPS.py
@@ -1,10 +1,40 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def player(prev_play, opponent_history=[], play_order=[{}, {}, {}, {}, {}]):
 
-def player(prev_play, opponent_history=[]):
+    if not prev_play:
+        # Nova partida: limpa tudo
+        opponent_history.clear()
+        for i in range(5):
+            play_order[i] = {}
+        prev_play = "R"
+
     opponent_history.append(prev_play)
 
+    # O que ganha de cada jogada
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+
+    # Tenta padrões de tamanho 5, 4, 3, 2, 1
+    for i, n in enumerate([5, 4, 3, 2, 1]):
+        if len(opponent_history) > n:
+            # Registra o padrão no dicionário correspondente
+            pattern = "".join(opponent_history[-n:])
+            play_order[i][pattern] = play_order[i].get(pattern, 0) + 1
+
+    # Usa o maior padrão que tiver dados para prever
+    for i, n in enumerate([5, 4, 3, 2, 1]):
+        if len(opponent_history) > n:
+            last = "".join(opponent_history[-(n - 1):]) if n > 1 else ""
+            potential_plays = [last + "R", last + "P", last + "S"]
+
+            sub_order = {k: play_order[i].get(k, 0) for k in potential_plays}
+
+            if max(sub_order.values()) > 0:
+                prediction = max(sub_order, key=sub_order.get)[-1]
+                # Contra-ataca: o que ganha da jogada prevista
+                guess = ideal_response[prediction]
+                break
 
     return guess
+
+
